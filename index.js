d71c0a90bb8d993012d918178486d7ddaf86e3bf
Jennifer Vargas <jevargasv1990@gmail.com>
2019-09-22 15:42:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,16 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]:value});
 }
 
- 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key){
+  return delete object.key;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key];
+  return object;
+}
